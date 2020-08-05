b6903555c740a099bf1e74cd9ec79f49fe9e87aa
linpf <lin.pifeng@gmail.com>
2020-08-04 21:44:50 -0700

    new updates for heroku deployment

--- clock.py
+++ clock.py
@@ -1,13 +1,23 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
+import requests
 
 sched = BlockingScheduler()
 
-@sched.scheduled_job('interval', minutes=3)
+@sched.scheduled_job('interval', minutes=10)
 def timed_job():
-    print('This job is run every three minutes.')
+    print('This job is run every ten minutes.')
 
 @sched.scheduled_job('cron', day_of_week='mon-fri', hour=17)
 def scheduled_job():
     print('This job is run every weekday at 5pm.')
+    url = 'http://www.bccdc.ca/Health-Info-Site/Documents/BCCDC_COVID19_Dashboard_Case_Details.csv'
+    r = requests.get(url)
+    with open('data/BCCDC_COVID19_Dashboard_Case_Details.csv', 'wb') as f:
+        f.write(r.content)
+
+    url = 'http://www.bccdc.ca/Health-Info-Site/Documents/BCCDC_COVID19_Dashboard_Lab_Information.csv'
+    r = requests.get(url)
+    with open('data/BCCDC_COVID19_Dashboard_Lab_Information.csv', 'wb') as f:
+        f.write(r.content)
 
 sched.start()
--- exp/update_bccbc.py
+++ exp/update_bccbc.py
@@ -2,10 +2,10 @@ import requests
 
 url = 'http://www.bccdc.ca/Health-Info-Site/Documents/BCCDC_COVID19_Dashboard_Case_Details.csv'
 r = requests.get(url)
-with open('../data/BCCDC_COVID19_Dashboard_Case_Details.csv', 'wb') as f:
+with open('data/BCCDC_COVID19_Dashboard_Case_Details.csv', 'wb') as f:
     f.write(r.content)
 
 url = 'http://www.bccdc.ca/Health-Info-Site/Documents/BCCDC_COVID19_Dashboard_Lab_Information.csv'
 r = requests.get(url)
-with open('../data//BCCDC_COVID19_Dashboard_Lab_Information.csv', 'wb') as f:
+with open('data//BCCDC_COVID19_Dashboard_Lab_Information.csv', 'wb') as f:
     f.write(r.content)
